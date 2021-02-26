587dd4dc8794f46c074af37cdce5b6bf913a54a5
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-02-26 10:43:44 +0900

    例外とerrorの確認

--- src/main/java/portfolio_backend/Controller/TechniqueController.java
+++ src/main/java/portfolio_backend/Controller/TechniqueController.java
@@ -27,7 +27,19 @@ public class TechniqueController {
 
     @PostMapping("/post")
     public void postTechnique(@RequestBody Technique technique) {
-        techniqueService.insert(technique);
-    }
+        try{
+            techniqueService.insert(technique);
+        } catch (Exception e) {
 
+            // どういう例外が発生しているか出力する
+            System.out.println("例外クラス: " + e.getClass().getName());
+
+            // 原因となった例外のチェーンを出力する
+            Throwable cause = e;
+            while ((cause = cause.getCause()) != null) {
+                System.out.println("原因例外クラス: " + cause.getClass().getName());
+            }
+            throw e;
+        }
+    }
 }
